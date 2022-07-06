3065409da80e76ad65017aa5eea486ab121eb197
Joyesh Debnath <itsmeJoyeshdebnath26@gmail.com>
2022-07-06 22:28:35 +0530

    registertaion logic is implemented on frontnend using user reducer and useraction and implemented on registration form

--- src/actions/userAction.js
+++ src/actions/userAction.js
@@ -4,6 +4,9 @@ import {
 	LOGIN_SUCCESS,
 	LOGIN_FAIL,
 	CLEAR_ERRORS,
+	REGISTER_USER_REQUEST,
+	REGISTER_USER_SUCCESS,
+	REGISTER_USER_FAIL,
 } from "../constants/userConstants";
 import axios from "axios";
 
@@ -25,6 +28,23 @@ export const login = (email, password) => async (dispatch) => {
 		dispatch({ type: LOGIN_FAIL, payload: error.response.data.message });
 	}
 };
+
+//for registration of user
+
+export const register = (userData) => async (dispatch) => {
+	try {
+		dispatch({ type: REGISTER_USER_REQUEST });
+		const config = { headers: { "Content-Type": "multipart/form-data" } };
+		const { data } = await axios.post(`/api/v1/register`, userData, config);
+		dispatch({ type: REGISTER_USER_SUCCESS, payload: data.user });
+	} catch (error) {
+		dispatch({
+			type: REGISTER_USER_FAIL,
+			payload: error.response.data.message,
+		});
+	}
+};
+
 //clearing errors
 export const clearErrors = () => async (dispatch) => {
 	dispatch({
--- src/component/User/LoginSignUp.js
+++ src/component/User/LoginSignUp.js
@@ -260,7 +260,7 @@ import MailOutlineIcon from "@material-ui/icons/MailOutline";
 import LockOpenIcon from "@material-ui/icons/LockOpen";
 import FaceIcon from "@material-ui/icons/Face";
 import { useDispatch, useSelector } from "react-redux";
-import { clearErrors, login } from "../../actions/userAction";
+import { clearErrors, login, register } from "../../actions/userAction";
 import { useAlert } from "react-alert";
 
 const LoginSignUp = ({ history, location }) => {
@@ -303,7 +303,7 @@ const LoginSignUp = ({ history, location }) => {
 		myForm.set("email", email);
 		myForm.set("password", password);
 		myForm.set("avatar", avatar);
-		// dispatch(register(myForm));
+		dispatch(register(myForm));
 	};
 
 	const registerDataChange = (e) => {
